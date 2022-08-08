d6c78cb6b22f0e38eee8ce974440821aa6e472e4
brianko <kobj11@gmail.com>
2022-08-08 14:54:10 -0700

    state established for post

--- phase-2-project/src/components/Post.js
+++ phase-2-project/src/components/Post.js
@@ -1,9 +1,14 @@
 import React, { useContext } from "react";
+import { useState } from "react";
 import { QueryContext } from "../context/query";
 
 function Post() {
 
     const { query, setQuery } = useContext(QueryContext)
+    const [ name, setName ] = useState("")
+    const [ address, setAddress ] =useState("")
+    const [ directions, setDirections ] = useState("")
+
     return (
         <form /*onSubmit={handleSubmit}*/>
         <h2>Add a Commode</h2>
@@ -11,17 +16,26 @@ function Post() {
         <input
             type="text"
             required
-            /*id="name"*/
-            /*value={name}*/
-            /*onChange={(e) => setDescription(e.target.value)}*/
+            value={name}
+            onChange={(e) => setName(e.target.value)}
         />
          <label htmlFor="Address">Address:</label>
         <textarea 
             required
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
         ></textarea>
         <label htmlFor="Directions">Directions</label>
-        <textarea></textarea>
+        <textarea
+            value={directions}
+            onChange={(e) => setDirections(e.target.value)}
+        ></textarea>
         <button type="submit">Add</button>
+        <p>
+            {name}
+            {address}
+            {directions}
+        </p>
     </form>
     )
 }
