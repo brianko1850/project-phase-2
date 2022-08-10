23469c57c5eacf93dfb4cb134e6aa655cfb95fe9
brianko <kobj11@gmail.com>
2022-08-10 15:59:28 -0700

    added bathroom obj

--- phase-2-project/src/components/Post.js
+++ phase-2-project/src/components/Post.js
@@ -8,9 +8,31 @@ function Post() {
     const [ name, setName ] = useState("")
     const [ address, setAddress ] =useState("")
     const [ directions, setDirections ] = useState("")
+    const bathroom = 
+        {
+          "id": 0,
+          "name": name,
+          "street": address,
+          "city": query,
+          "state": "",
+          "accessible": true,
+          "unisex": true,
+          "directions": directions,
+          "comment": "",
+          "latitude": 47.5826887,
+          "longitude": -122.3846954,
+          "created_at": Date.now(),
+          "updated_at": Date.now(),
+          "downvote": 0,
+          "upvote": 0,
+          "country": "",
+          "changing_table": false,
+          "edit_id": 0,
+          "approved": false
+        }
 
     return (
-        <form /*onSubmit={handleSubmit}*/>
+        <form onSubmit={console.log(bathroom)}>
         <h2>Add a Commode</h2>
         <label htmlFor="Name">Name:</label>
         <input
