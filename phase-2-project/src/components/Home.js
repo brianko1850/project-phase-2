9afca56e3508acac3065fd93c60bb72d4137a85c
brianko <kobj11@gmail.com>
2022-09-02 10:36:49 -0700

    make it pretty

--- phase-2-project/src/components/Home.js
+++ phase-2-project/src/components/Home.js
@@ -1,22 +1,22 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { QueryContext } from "../context/query";
 import './style.css'
 
 
 function Home({ bathrooms }) {
 
-    const { query, setQuery } = useContext(QueryContext)
+    const { setQuery } = useContext(QueryContext)
     const disiplayBathroom = bathrooms.map(bathroom=> {
         return (
-        <div>
-            <ul>
-                <h2>{bathroom.name}</h2>
-                <li>{bathroom.street}</li>
-                <li>{bathroom.directions}</li>
-            </ul>
-            <br/>
-        </div>
-            )
+            <div>
+                <ul>
+                    <h2>{bathroom.name}</h2>
+                    <li>{bathroom.street}</li>
+                    <li>{bathroom.directions}</li>
+                </ul>
+                <br/>
+            </div>
+        )
     })
     const handleSearch=(e)=>{
         e.preventDefault();
@@ -24,19 +24,19 @@ function Home({ bathrooms }) {
     }
   
     return (
-    <>
-        <h1>Santuary Potties</h1>
-            <form onSubmit={ handleSearch }>
-                <input
-                    type="text"
-                    id="header-search"
-                    placeholder="Search cities"
-                    name="s" 
-                />
-                <button type="submit">Search</button>
-            </form>
-           {disiplayBathroom}        
-    </>
+        <>
+            <h1>Santuary Potties</h1>
+                <form onSubmit={ handleSearch }>
+                    <input
+                        type="text"
+                        id="header-search"
+                        placeholder="Search cities"
+                        name="s" 
+                    />
+                    <button type="submit">Search</button>
+                </form>
+            {disiplayBathroom}        
+        </>
     )
 }
 
